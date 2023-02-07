1668e22b4933759f3a85324e06703a2da6a76ff3
markus.fischboeck <markus.fischboeck@mytaxi.com>
2023-02-07 21:36:31 +0100

    Added support for Alignment.RIGHT

--- src/main/kotlin/net/fischboeck/splitflap/util/DisplayFormatter.kt
+++ src/main/kotlin/net/fischboeck/splitflap/util/DisplayFormatter.kt
@@ -1,6 +1,7 @@
 package net.fischboeck.splitflap.util
 
 import mu.KLogging
+import java.util.ArrayDeque
 
 object DisplayFormatterBuilder {
 
@@ -10,61 +11,113 @@ object DisplayFormatterBuilder {
 
     class Insertion(val text: String, val alignment: Alignment)
 
-    class DisplayFormatter {
-        private val dimX: Short
-        private val dimY: Short
-        private val whitespace: String
-        private val jobs: java.util.ArrayDeque<Insertion>
+    class DisplayFormatter internal constructor(private val dimX: Short, private val dimY: Short, private val whitespace: String) {
 
-        internal constructor(dimX: Short, dimY: Short, whitespace: String) {
-            this.dimX = dimX
-            this.dimY = dimY
-            this.whitespace = whitespace
-            this.jobs = java.util.ArrayDeque()
+        private val lines = (0 until dimY).map { ArrayDeque <Insertion>() }.toList()
+        private var current = 0;
+
+        fun newLine(): DisplayFormatter {
+            current++
+            return this
         }
 
-        fun append(text: String, alignment: Alignment): DisplayFormatter {
-            if (jobs.size > 4) {
-                return this;
+        fun append(text: String, alignment: Alignment = Alignment.LEFT): DisplayFormatter {
+
+            if (current > lines.size) {
+                return this
             }
-            jobs.addLast(Insertion(text, alignment))
+            val sanitizedText = sanitize(text)
+            lines[current].addLast(Insertion(sanitizedText, alignment))
+
+            // ensures text with alignment.RIGHT is the last one on this line
+            // this way it's easier when collapsing
+            if (alignment == Alignment.RIGHT) {
+                current++
+            }
             return this
         }
 
         fun build(): String {
             val buffer = StringBuilder()
-
-            while(jobs.isNotEmpty()) {
-                val current = jobs.poll()
-
-                if (current.alignment == Alignment.LEFT) {
-                    // append to buffer
-                    buffer.append(padRight(current.text))
+            lines.map {
+                collapse(it)
+            }.forEach {
+                if (it.length > dimX) {
+                    buffer.append(it.substring(0 until dimX))
+                } else {
+                    buffer.append(it)
                 }
             }
-
-            while(buffer.length < dimX * dimY) {
-                buffer.append(whitespace)
-            }
-
             return buffer.toString()
         }
 
-        private fun padRight(text: String): String {
-            return if (text.length > dimX) {
-                text.substring(0 until dimX)
-            } else {
-                val builder = StringBuilder()
-                builder.append(text)
-                for (i in 0 until dimX - text.length) {
-                    builder.append(whitespace)
+        private fun collapse(insertions: ArrayDeque<Insertion>): String {
+
+            var result = ""
+            while (insertions.isNotEmpty()) {
+
+                val item = insertions.poll()
+                val next = insertions.peek()
+
+                if (next != null && next.alignment == Alignment.LEFT) {
+                    result += item.text
                 }
-                builder.toString()
+
+                if (next != null && next.alignment == Alignment.RIGHT) {
+
+                    result += item.text
+
+                    // pad until result.length + next.text.length = dimX -1
+                    val pad = dimX - (result.length + next.text.length)
+                    (0 until pad).forEach { _ -> result += whitespace }
+                    result += next.text
+                    return result
+                }
+
+                if (next == null && item.alignment == Alignment.LEFT) {
+                    result += item.text
+                    return padRight(result)
+                }
+
+                if (next == null && item.alignment == Alignment.RIGHT) {
+                    val padLeft = dimX - item.text.length
+                    (0 until padLeft).forEach { _ -> result += whitespace }
+                    result += item.text
+                }
             }
+
+            return padRight(result)
+        }
+
+
+        private fun padRight(text: String): String {
+
+            val builder = StringBuilder()
+            builder.append(text)
+            for (i in 0 until dimX - text.length) {
+                builder.append(whitespace)
+            }
+            return builder.toString()
+        }
+
+
+        private fun sanitize(text: String): String {
+
+            var result = text.uppercase()
+
+            // replace common characters
+            result = result.replace("Ö", "OE")
+            result = result.replace("Ä", "AE")
+            result = result.replace("Ü", "UE")
+
+            // trim to length
+            if (result.length > dimX) {
+                result = result.substring(0 until dimX.toInt())
+            }
+            return result
         }
 
         companion object: KLogging()
-
     }
 }
 
--- src/test/kotlin/net/fischboeck/splitflap/util/DisplayFormatterTest.kt
+++ src/test/kotlin/net/fischboeck/splitflap/util/DisplayFormatterTest.kt
@@ -2,13 +2,14 @@ package net.fischboeck.splitflap.util
 
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.math.exp
 
 class DisplayFormatterTest {
 
     @Test
     fun `no text appended`() {
 
-        val formatter = DisplayFormatterBuilder.newBuilder(15, 4)
+        val formatter = DisplayFormatterBuilder.newBuilder(15, 4, "_")
         val result = formatter.build()
 
         val expectedBuffer = StringBuffer()
@@ -21,8 +22,8 @@ class DisplayFormatterTest {
     @Test
     fun `left aligned text is padding correctly`() {
 
-        val result = DisplayFormatterBuilder.newBuilder(15, 4)
-            .append("FOOBAR", Alignment.LEFT)
+        val result = DisplayFormatterBuilder.newBuilder(15, 4, "_")
+            .append("FOOBAR")
             .build()
         val expected = "FOOBAR______________________________________________________"
 
@@ -32,11 +33,11 @@ class DisplayFormatterTest {
     @Test
     fun `can write 4 left aligned rows correctly`() {
 
-        val result = DisplayFormatterBuilder.newBuilder(15, 4)
-            .append("FOO", Alignment.LEFT)
-            .append("BAR", Alignment.LEFT)
-            .append("BUZZ", Alignment.LEFT)
-            .append("BAMBOO", Alignment.LEFT)
+        val result = DisplayFormatterBuilder.newBuilder(15, 4, "_")
+            .append("FOO").newLine()
+            .append("BAR").newLine()
+            .append("BUZZ").newLine()
+            .append("BAMBOO").newLine()
             .build()
         val expected = "FOO____________BAR____________BUZZ___________BAMBOO_________"
         assertEquals(expected, result)
@@ -45,10 +46,72 @@ class DisplayFormatterTest {
     @Test
     fun `truncates left padded word if exceeding line`() {
 
-        val result = DisplayFormatterBuilder.newBuilder(15, 4)
+        val result = DisplayFormatterBuilder.newBuilder(15, 4, "_")
             .append("FOOBARFOOBARFOOBAR", Alignment.LEFT)
             .build()
         val expected = "FOOBARFOOBARFOO_____________________________________________"
         assertEquals(expected, result)
     }
+
+    @Test
+    fun `single right aligned word`() {
+
+        val result = DisplayFormatterBuilder.newBuilder(5, 3, "_")
+            .append("XYZ", Alignment.RIGHT)
+            .build()
+        val expected = "__XYZ__________"
+
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `four right aligned words`() {
+
+        val result = DisplayFormatterBuilder.newBuilder(5, 4, "_")
+            .append("XY", Alignment.RIGHT)
+            .append("XY", Alignment.RIGHT)
+            .append("YZ", Alignment.RIGHT)
+            .append("12", Alignment.RIGHT)
+            .build()
+
+        val expected = "___XY___XY___YZ___12"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `left right left right mixed lines`() {
+
+        val result = DisplayFormatterBuilder.newBuilder(6, 4, "_")
+            .append("12").newLine()
+            .append("34", Alignment.RIGHT)
+            .append("56").newLine()
+            .append("78", Alignment.RIGHT)
+            .build()
+
+        val expected = "12________3456________78"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `left right on same line`() {
+
+        val result = DisplayFormatterBuilder.newBuilder(8, 2, "_")
+            .append("ABC").append("ABC", Alignment.RIGHT)
+            .append("CCC").append("CCC", Alignment.RIGHT)
+            .build()
+
+        val expected = "ABC__ABCCCC__CCC"
+        assertEquals(expected, result)
+    }
+
+    @Test
+    fun `truncates right alignment if space exceeds`() {
+
+        val result = DisplayFormatterBuilder.newBuilder(8, 1, "_")
+            .append("HELLO ").append("WORLD", Alignment.RIGHT)
+            .build()
+
+        assertEquals("HELLO WO", result)
+    }
+
 }
